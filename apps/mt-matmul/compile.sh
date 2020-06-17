dd74613b38cd9d17b1c730228fdbfe7cfaa16c73
Borja Perez <borja.perez1@bsc.es>
2020-06-17 15:04:27 +0200

    Modified matmul execution and compilation

--- apps/mt-matmul/compile.sh
+++ apps/mt-matmul/compile.sh
@@ -1,1 +1,14 @@
-riscv64-unknown-elf-gcc -I../common/env -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gcv ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld matmul.c mt-matmul.c -o matmul
+#/bin/bash
+
+if [ "$#" -gt 2 ] || [ "$#" -lt 1 ]; then
+    echo "Usage: #cores [opt]Mat_dim(power of 2)"
+    exit 2
+fi
+
+if [ "$#" -eq 2 ]; then
+    perl matmul_gendata.pl --size $2 > dataset.h
+fi
+
+sed -i '/li a7/c li a7, '$1 ./my_crt.S
+
+riscv64-unknown-elf-gcc -I -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gcv ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld matmul.c mt-matmul.c -o matmul
--- apps/mt-matmul/run_all.sh
+++ apps/mt-matmul/run_all.sh
@@ -1,14 +1,7 @@
 #/bin/bash!
-
 if [ "$#" -gt 2 ] || [ "$#" -lt 1 ]; then
     echo "Usage: #cores [opt]Mat_dim(power of 2)"
     exit 2
 fi
-
-if [ "$#" -eq 2 ]; then
-    perl matmul_gendata.pl --size $2 > dataset.h
-fi
-
-sed -i '/li a7/c li a7, '$1 ./my_crt.S
-sh compile.sh
-/home/bscuser/local/riscvv08/gnu/bin/spike --ic=1:1:8 --dc=8:8:64 --isa=RV64IMAFDCV -p$1 matmul
+sh compile.sh $1 $2
+spike --ic=1:1:8 --dc=8:8:64 --isa=RV64IMAFDCV -p$1 matmul
