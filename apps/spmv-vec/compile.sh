79ca0ca0d9e269e33091887f3a8d74206115798f
Borja Perez <borja.perez1@bsc.es>
2020-11-26 11:49:29 +0100

    Corrected a bug in SpMV

--- apps/spmv-vec/compile.sh
+++ apps/spmv-vec/compile.sh
@@ -14,6 +14,6 @@ sed -i '/(\b|[^ABC])n =/c n = '$2';' ./spmv.c
 
 systemctl is-active --quiet docker || sudo service docker start #If docker inactive, activate
 
-sudo docker run -v$PWD/..:/tmp --rm epi_compiler_docker /bin/bash -c "cd /tmp/SpMV; /llvm-EPI-development-toolchain-cross/bin/clang --target=riscv64-unknown-elf -mepi -I../common/env -I../common -I/usr/include/ -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -fno-tree-vectorize -march=rv64gc ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -T ../common/test.ld spmv.c -o spmv"
+sudo docker run -v$PWD/..:/tmp --rm epi_compiler_docker /bin/bash -c "cd /tmp/spmv-vec; /llvm-EPI-development-toolchain-cross/bin/clang --target=riscv64-unknown-elf -mepi -I../common/env -I../common -I/usr/include/ -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -fno-tree-vectorize -march=rv64gc ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -T ../common/test.ld spmv.c -o spmv"
 
 #riscv64-unknown-elf-gcc -I../common/env -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gcv ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld spmv.c -o spmv
