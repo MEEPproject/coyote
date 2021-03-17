270c8fbbef1f75231804d6f8ed3bdde711856119
Alexander Fell <alexander.fell@bsc.es>
2021-03-17 20:12:17 +0100

    Added some comments to the compile.sh in matmul

--- apps/mt-matmul/compile.sh
+++ apps/mt-matmul/compile.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 if [ "$#" -gt 2 ] || [ "$#" -lt 1 ]; then
     echo "Usage: #cores [opt]Mat_dim(power of 2)"
@@ -11,4 +11,16 @@ fi
 
 sed -i '/li a7/c li a7, '$1 ./my_crt.S
 
-riscv64-unknown-elf-gcc -I../common/env -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gcv ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld matmul.c mt-matmul.c -o matmul
+
+#-- march parameter settings
+#   https://www.sifive.com/blog/all-aboard-part-1-compiler-args
+#
+#    M: Integer Multiplication and Division
+#    A: Atomic Instructions
+#    F: Single-Precision Floating-Point
+#    D: Double-Precision Floating-Point
+#    C: Compressed Instructions
+
+
+#riscv64-unknown-elf-gcc -I../common/env -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gcv ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld matmul.c mt-matmul.c -o matmul
+riscv64-unknown-elf-gcc -I../common/env -I../common -DPREALLOCATE=1 -mcmodel=medany -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64imafdc ../common/syscalls.c ./my_crt.S -static -nostdlib -nostartfiles -lm -lgcc -T ../common/test.ld matmul.c mt-matmul.c -o matmul
