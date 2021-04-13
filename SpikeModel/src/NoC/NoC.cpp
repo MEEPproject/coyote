561664bf9b7642d81ee927d1c840d4f07a144842
Mariano Benito <mariano.benito1@bsc.es>
2021-04-13 15:57:14 +0200

    Add basic NoC statistics (load as pkt/node/cycle and the number of packets processed of each type).

--- SpikeModel/src/NoC/NoC.cpp
+++ SpikeModel/src/NoC/NoC.cpp
@@ -17,44 +17,52 @@ namespace spike_model
         num_tiles_(params->num_tiles),
         num_memory_cpus_(params->num_memory_cpus)
     {
-            for(uint16_t i=0; i<num_tiles_; i++)
-            {
-                std::string out_name=std::string("out_tile") + sparta::utils::uint32_to_str(i);
-                std::unique_ptr<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> out=std::make_unique<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, out_name);
-                out_ports_tiles_[i]=std::move(out);
+        for(uint16_t i=0; i<num_tiles_; i++)
+        {
+            std::string out_name=std::string("out_tile") + sparta::utils::uint32_to_str(i);
+            std::unique_ptr<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> out=std::make_unique<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, out_name);
+            out_ports_tiles_[i]=std::move(out);
 
-                std::string in_name=std::string("in_tile") + sparta::utils::uint32_to_str(i);
-                std::unique_ptr<sparta::DataInPort<std::shared_ptr<NoCMessage>>> in=std::make_unique<sparta::DataInPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, in_name);
-                in_ports_tiles_[i]=std::move(in);
-                in_ports_tiles_[i]->registerConsumerHandler(CREATE_SPARTA_HANDLER_WITH_DATA(NoC, handleMessageFromTile_, std::shared_ptr<NoCMessage>));
-            }
+            std::string in_name=std::string("in_tile") + sparta::utils::uint32_to_str(i);
+            std::unique_ptr<sparta::DataInPort<std::shared_ptr<NoCMessage>>> in=std::make_unique<sparta::DataInPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, in_name);
+            in_ports_tiles_[i]=std::move(in);
+            in_ports_tiles_[i]->registerConsumerHandler(CREATE_SPARTA_HANDLER_WITH_DATA(NoC, handleMessageFromTile_, std::shared_ptr<NoCMessage>));
+        }
 
-            for(uint16_t i=0; i<num_memory_cpus_; i++)
-            {
-                std::string out_name=std::string("out_memory_cpu") + sparta::utils::uint32_to_str(i);
-                std::unique_ptr<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> out=std::make_unique<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, out_name);
-                out_ports_memory_cpus_[i]=std::move(out);
+        for(uint16_t i=0; i<num_memory_cpus_; i++)
+        {
+            std::string out_name=std::string("out_memory_cpu") + sparta::utils::uint32_to_str(i);
+            std::unique_ptr<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> out=std::make_unique<sparta::DataOutPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, out_name);
+            out_ports_memory_cpus_[i]=std::move(out);
 
-                std::string in_name=std::string("in_memory_cpu") + sparta::utils::uint32_to_str(i);
-                std::unique_ptr<sparta::DataInPort<std::shared_ptr<NoCMessage>>> in=std::make_unique<sparta::DataInPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, in_name);
-                in_ports_memory_cpus_[i]=std::move(in);
-                in_ports_memory_cpus_[i]->registerConsumerHandler(CREATE_SPARTA_HANDLER_WITH_DATA(NoC, handleMessageFromMemoryCPU_, std::shared_ptr<NoCMessage>));
-            }
+            std::string in_name=std::string("in_memory_cpu") + sparta::utils::uint32_to_str(i);
+            std::unique_ptr<sparta::DataInPort<std::shared_ptr<NoCMessage>>> in=std::make_unique<sparta::DataInPort<std::shared_ptr<NoCMessage>>> (&unit_port_set_, in_name);
+            in_ports_memory_cpus_[i]=std::move(in);
+            in_ports_memory_cpus_[i]->registerConsumerHandler(CREATE_SPARTA_HANDLER_WITH_DATA(NoC, handleMessageFromMemoryCPU_, std::shared_ptr<NoCMessage>));
+        }
     }
 
     void NoC::handleMessageFromTile_(const std::shared_ptr<NoCMessage> & mess)
     {
         count_rx_packets_++;
+        count_tx_packets_++;
         switch(mess->getType())
         {
             case NoCMessageType::REMOTE_L2_REQUEST:
-            case NoCMessageType::MEMORY_REQUEST:
-            case NoCMessageType::REMOTE_L2_ACK:
-            case NoCMessageType::MCPU_REQUEST:
-            case NoCMessageType::SCRATCHPAD_ACK:
-                count_tx_packets_++;
+                count_remote_l2_requests_++;
                 break;
-
+            case NoCMessageType::MEMORY_REQUEST:
+                count_memory_requests_++;
+                break;
+            case NoCMessageType::REMOTE_L2_ACK:
+                count_remote_l2_acks_++;
+                break;
+            case NoCMessageType::MCPU_REQUEST:
+                count_mcpu_requests_++;
+                break;
+            case NoCMessageType::SCRATCHPAD_ACK:
+                count_scratchpad_acks_++;
+                break;
             default:
                 sparta_assert(false, "Unsupported message received from a Tile!!!");
         }
@@ -63,13 +71,15 @@ namespace spike_model
     void NoC::handleMessageFromMemoryCPU_(const std::shared_ptr<NoCMessage> & mess)
     {
         count_rx_packets_++;
+        count_tx_packets_++;
         switch(mess->getType())
         {
             case NoCMessageType::MEMORY_ACK:
-            case NoCMessageType::MCPU_REQUEST:
-                count_tx_packets_++;
+                count_memory_acks_++;
                 break;
-
+            case NoCMessageType::MCPU_REQUEST:
+                count_mcpu_requests_++;
+                break;
             default:
                 sparta_assert(false, "Unsupported message received from a Memory Controller!!!");
         }
--- SpikeModel/src/NoC/NoC.hpp
+++ SpikeModel/src/NoC/NoC.hpp
@@ -31,7 +31,7 @@ namespace spike_model
     {
         /*!
          * \class spike_model::NoC
-         * \brief NoC models the network on chip interconnecting tiles and memory controllers
+         * \brief NoC models the network on chip interconnecting tiles and memory cpus
          * 
          * ABC to define the interface between PEs and NoC
          */
@@ -106,6 +106,59 @@ namespace spike_model
             "Number of packets sent",       // description
             sparta::Counter::COUNT_NORMAL   // behavior
         );                                  //! The number of packets sent by the NoC
+
+    private:
+
+        sparta::StatisticDef load_pkt_ = sparta::StatisticDef
+        (
+            getStatisticSet(),              // parent
+            "load_pkt",                     // name
+            "NoC load (pkt/node/cycle)",    // description
+            getStatisticSet(),              // context
+            "received_packets/("+std::to_string(num_tiles_+num_memory_cpus_)+"*cycles)" // Expression
+        );                                  //! Basic NoC load as packets/cycles
+        sparta::Counter count_remote_l2_requests_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "remote_l2_requests",           // name
+            "Number of Remote_L2_Request",  // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of Remote_L2_Requests forwarded by NoC
+        sparta::Counter count_remote_l2_acks_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "remote_l2_acks",               // name
+            "Number of Remote_L2_Acks",     // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of Remote_L2_Acks forwarded by NoC
+        sparta::Counter count_memory_requests_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "memory_requests",              // name
+            "Number of Memory_Request",     // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of Memory_Requests forwarded by NoC
+        sparta::Counter count_memory_acks_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "memory_acks",                  // name
+            "Number of Memory_Acks",        // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of Memory_Acks forwarded by NoC
+        sparta::Counter count_mcpu_requests_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "mcpu_requests",                // name
+            "Number of MCPU_Requests",      // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of MCPU_Requests forwarded by NoC
+        sparta::Counter count_scratchpad_acks_ = sparta::Counter
+        (
+            getStatisticSet(),              // parent
+            "scratchpad_acks",              // name
+            "Number of Scratchpad_Acks",    // description
+            sparta::Counter::COUNT_NORMAL   // behavior
+        );                                  //! The number of Scratchpad_Acks forwarded by NoC
 
     };
 
