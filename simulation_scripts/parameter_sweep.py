864ea58baaed2253b6f506deee3b9c85e1c7b82f
Borja Perez <bpere1@bsc.es>
2021-05-14 13:54:35 +0200

    Updates the script to perform parameter sweeps

--- simulation_scripts/parameter_sweep.py
+++ simulation_scripts/parameter_sweep.py
@@ -7,31 +7,37 @@ coyote_bin_path="../SpikeModel/build/spike_model"
 apps_path="../apps/"
 apps=["mt-matmul-vec/matmul"]
 
-if len(sys.argv)<5 and len(sys.argv)<6:
+if len(sys.argv)<4 and len(sys.argv)>5:
     print(len(sys.argv))
     print("Usage: python3 parameter_sweep.py config_file parameter_name value_list(brace-enclosed, comma-separated) [(Optional)enable_trace]")
     sys.exit()
 
 save_trace=False
-if len(sys.argv)==6:
+if len(sys.argv)==5:
     if sys.argv[5]!="enable_trace":
         print("Unknown parameter "+sys.argv[4])
         sys.exit()
     else:
         save_trace=True
 
-config_file=sys.argv[2]
-param=sys.argv[3]
-values=sys.argv[4][1:-1].split(',')
+config_file=sys.argv[1]
+param=sys.argv[2]
+
+print("Sweeping param "+param)
+print("Apps: "+str(apps)+"\n")
+
+values=sys.argv[3][1:-1].split(',')
 
 out_path="./results_"+param.split(".")[-1]+"/"
 
 subprocess.run(["mkdir", out_path])
 
 for app in apps:
+    print(app+":")
     app_name=app.split("/")[-1]
     subprocess.run(["mkdir", out_path+app_name])
     for v in values:
+        print("\t"+v)
         command=[coyote_bin_path, "-c",  config_file, "-p", "meta.params.cmd", apps_path+app, "-p", param, v]
         if save_trace:
             command.append("-p")
