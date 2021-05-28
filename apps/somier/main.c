6be8529da500cd29f56edefa624dd627310fd5ed
Mariano Benito <mariano.benito1@bsc.es>
2021-05-28 14:20:52 +0200

    Add the posibility to use multiple cores in simulation script and minor fixes

--- apps/somier/main.c
+++ apps/somier/main.c
@@ -22,7 +22,7 @@ int err;
 int thread_entry(int cid, int nc)
 {
    int ntsteps;
-ntsteps = 2;
+ntsteps = 3;
 
   // printf ("Problem size = %d, steps = %d\n", N, ntsteps);
 
--- simulation_scripts/parameter_sweep.py
+++ simulation_scripts/parameter_sweep.py
@@ -1,24 +1,33 @@
+#!/usr/bin/env python3
 #Run using python3
 
 import sys
 import subprocess
+import queue
+import time
 
+# Configuration
 coyote_bin_path="../SpikeModel/build/spike_model"
 apps_path="../apps/"
 apps=["mt-matmul-vec/matmul"]
+cores=1 # Change to the number of procs to use
+polling_time=60 # seconds
 
+# Params validation
 if len(sys.argv)<4 and len(sys.argv)>5:
     print(len(sys.argv))
     print("Usage: python3 parameter_sweep.py config_file parameter_name value_list(brace-enclosed, comma-separated) [(Optional)enable_trace]")
     sys.exit()
 
+# Trace?
 save_trace=False
 if len(sys.argv)==5:
-    if sys.argv[5]!="enable_trace":
+    if sys.argv[4]!="enable_trace":
         print("Unknown parameter "+sys.argv[4])
         sys.exit()
     else:
         save_trace=True
+        cores=1
 
 config_file=sys.argv[1]
 param=sys.argv[2]
@@ -26,27 +35,66 @@ param=sys.argv[2]
 print("Sweeping param "+param)
 print("Apps: "+str(apps)+"\n")
 
+# Get values for sweeping
 values=sys.argv[3][1:-1].split(',')
-
+# Generate out path
 out_path="./results_"+param.split(".")[-1]+"/"
+# Create variable results folder
+subprocess.run(["mkdir", "-p", out_path]) 
 
-subprocess.run(["mkdir", out_path])
-
+# Enqueue (FIFO) commands and files
+commands = queue.Queue()
+outs = queue.Queue()
+errs = queue.Queue()
 for app in apps:
-    print(app+":")
     app_name=app.split("/")[-1]
-    subprocess.run(["mkdir", out_path+app_name])
+    # Create app results folder
+    subprocess.run(["mkdir", "-p", out_path+app_name])
+    # Copy configuration file to app results folder
+    subprocess.run(["cp", config_file, out_path+app_name])
     for v in values:
-        print("\t"+v)
-        command=[coyote_bin_path, "-c",  config_file, "-p", "meta.params.cmd", apps_path+app, "-p", param, v]
+        command=["time", coyote_bin_path, "-c",  config_file, "-p", "meta.params.cmd", apps_path+app, "-p", param, v]
         if save_trace:
             command.append("-p")
             command.append("meta.params.trace")
             command.append("true")
-        out = open(out_path+app_name+"/out_"+v, "w")
-        subprocess.run(command, stdout=out)
+        commands.put(command)
+        outs.put(out_path+app_name+"/out_"+v)
+        errs.put(out_path+app_name+"/err_"+v)
+
+# Use multiple cores
+avail_cores = cores
+procs = []
+files = []
+while not commands.empty() or procs:
+    # Execute simulations
+    while not commands.empty() and avail_cores > 0:
+        avail_cores -= 1
+        command = commands.get()
+        out_f = outs.get()
+        err_f = errs.get()
+        out = open(out_f, "w")
+        err = open(err_f, "w")
+        app_name = command[6].split("/")[-1]
         if save_trace:
-            subprocess.run(["mv", "trace", out_path+app_name+"/trace_"+v])
+            value = command[-4]
+        else:
+            value = command[-1]
+        print("\tExecuting: " + app_name + " with value " + value)
+        proc = subprocess.Popen(command, stdout=out, stderr=err, stdin=subprocess.PIPE)
+        files.append(out)
+        files.append(err)
+        procs.append(proc)
+    # Polling
+    for p in procs:
+        if p.poll() is not None:
+            procs.remove(p)
+            avail_cores += 1
+            if save_trace: # Save trace runs only with 1 core in parallel
+                subprocess.run(["mv", "trace", out_path+app_name+"/trace_"+value])
+        else:
+            time.sleep(polling_time)
 
-        out.close()
-
+# Close all opened files
+for file in files:
+    file.close()
