accfe7de4ffc4843e885bddb0b6a474bd12a5d1f
Alexander Fell <alexander.fell@bsc.es>
2021-07-23 15:06:22 +0200

    WiP: Basic version of the Memory Tile

--- SpikeModel/src/MemoryTile/MemoryCPUWrapper.cpp
+++ SpikeModel/src/MemoryTile/MemoryCPUWrapper.cpp
@@ -24,7 +24,8 @@ namespace spike_model {
 		mes->getRequest()->handle(this);
 	}
 
-	//-- A transaction for the Cache
+
+	//-- Bypass for a scalar memory operation
 	void MemoryCPUWrapper::handle(std::shared_ptr<spike_model::CacheRequest> mes) {
 		//std::cout << "MCPU: Instruction for the MC received" << std::endl;
 		switch(mes->getType()) {
@@ -38,59 +39,163 @@ namespace spike_model {
 				count_store_++;
 				//std::cout << "Storing: " << mes << ", coreID: " << mes->getCoreId() << std::endl;
 				break;
-
 		}
-		out_port_mc_.send(mes, 0);
+		
+		//-- Schedule memory request for the MC
+		mem_req_pipeline.push(mes);
 	}
 
 	//-- A memory transaction to be handled by the MCPU
-        //Note that VVL of 0 is received when the simulation is setting up
-        //and the processor objects are created.
+	//   Note that VVL of 0 is received when the simulation is setting up
+	//   and the processor objects are created.
 	void MemoryCPUWrapper::handle(std::shared_ptr<spike_model::MCPUSetVVL> mes) {
-	    std::cout <<  "  AVL " << mes->getAVL() << " received from MCPU: Core "
-                      << mes->getCoreId() <<std::endl;
-            mcpu_req.push_back(mes);
-            issue_mcpu_event_.schedule(1);
+		std::cout <<  "  VVL " << mes->getVVL() << " received from MCPU: Core "
+						<< mes->getCoreId() <<std::endl;
+
+			//-- TODO: Compute AVL
+			vvl_ = mes->getAVL();
+			mes->setVVL(vvl_);
+			
+			//   Thread ID is not required currently
+			//-- TODO: Return the new VVL
 	}
 
-	//-- An instruction for the MCPU
-    void MemoryCPUWrapper::handle(std::shared_ptr<spike_model::MCPUInstruction> r) {
-        std::cout << "MCPU: Memory instruction received. Core: " << r->getCoreId() << ", BaseAddress: " << r->getBaseAddress() << ", Size: "
-                                                                            << (int) r->getWidth() << ", Is indexed? " << r->getIndexed()<< std::endl;
-        switch(r->getOperation()) {
-        	case MCPUInstruction::Operation::LOAD:
-        		std::cout << "\tIt is a load" << std::endl;
-        		break;
-        	case MCPUInstruction::Operation::STORE:
-        		std::cout << "\tIt is a store" << std::endl;
-        		break;
-        	default:
-        		std::cout << "MCPU: I do not know, what the core wants from me!" << std::endl;
-        }
-        //TODO: Something interesting :)
-        //Generate memory accesses/ scratchpad requests
-    }
+	//-- A vector instruction for the MCPU
+	void MemoryCPUWrapper::handle(std::shared_ptr<spike_model::MCPUInstruction> r) {
+		std::cout << "MCPU: Memory instruction received. Core: " << r->getCoreId() << ", BaseAddress: " << r->get_baseAddress() << ", Size: "
+					<< (int)r->get_width() << ", op: " << (int)r->get_operation() << ", sub_op: " << (int)r->get_suboperation() << std::endl;
+		
+		//-- schedule the incoming message
+		sched_incoming.push(r);
+		
+		if(mcpu_incoming_idle) {
+			controller_cycle_event_.schedule();
+			mcpu_incoming_idle = false;
+		}
+	}
 
 
+	void MemoryCPUWrapper::controllerCycle_() {
+		schedule_incoming_mem_ops();
+		//schedule_mem_ops_to_mc();
+		//schedule_outgoing_mem_ops();
+		
+		if(sched_incoming.size() == 0) {
+			mcpu_incoming_idle = true;
+		} else {
+			controller_cycle_event_.schedule(1);
+		}
+	}
+	
+	
+	void MemoryCPUWrapper::schedule_incoming_mem_ops() {
+		if(sched_incoming.size() == 0) {
+			return;
+		}
+		
+		std::shared_ptr<MCPUInstruction> instr_to_schedule = sched_incoming.front();
+		
+		switch(instr_to_schedule->get_suboperation()) {
+			case MCPUInstruction::SubOperation::UNIT:
+				memOp_unit(instr_to_schedule);
+				break;
+			case MCPUInstruction::SubOperation::NON_UNIT:
+				memOp_nonUnit(instr_to_schedule);
+				break;
+			case MCPUInstruction::SubOperation::ORDERED_INDEX:
+				memOp_orderedIndex(instr_to_schedule);
+				break;
+			case MCPUInstruction::SubOperation::UNORDERED_INDEX:
+				memOp_unorderedIndex(instr_to_schedule);
+				break;
+			default:
+				std::cerr << "UNKNOWN MCPUInstruction SubOperation. The MCPU does not understand, what that instruction means. Ignoring it." << std::endl;
+		}
+		
+		//-- consume the instruction from the scheduler
+		sched_incoming.pop();
+	}
+	
+	
+	void MemoryCPUWrapper::schedule_mem_ops_to_mc() {
+		if(mem_req_pipeline.size() == 0) {
+			return;
+		}
+		
+		//-- Get the oldest Cache Request from the queue
+		std::shared_ptr<CacheRequest> instr_for_mc = mem_req_pipeline.front();
+		
+		//-- Send to the MC
+		out_port_mc_.send(instr_for_mc, 0);
+	}
+	
+	
+	void MemoryCPUWrapper::schedule_outgoing_mem_ops() {
+		
+	}
+	
+	
+	
+	void MemoryCPUWrapper::memOp_unit(std::shared_ptr<MCPUInstruction> instr) {
+				
+		//-- get the number of elements loaded by 1 memory request
+		uint number_of_elements_per_request = line_size_ / (uint32_t)instr->get_width();
+		uint number_of_mem_ops = vvl_ / number_of_elements_per_request;
+		
+		if(number_of_mem_ops == 0) number_of_mem_ops=1;
+		
+		for(uint i=0; i<number_of_mem_ops; ++i) {
+			//-- Generate a cache line request
+			std::shared_ptr<CacheRequest> memory_request = std::make_shared<CacheRequest>(
+						instr->get_baseAddress()+i*64, 
+						(instr->get_operation() == MCPUInstruction::Operation::LOAD) ? 
+									CacheRequest::AccessType::LOAD : CacheRequest::AccessType::STORE,
+						0,
+						getClock()->currentCycle(), 
+						(uint16_t)-1);
+			
+			//-- schedule this request for the MC
+			mem_req_pipeline.push(memory_request);
+		}
+	}
+	
+	void MemoryCPUWrapper::memOp_nonUnit(std::shared_ptr<MCPUInstruction> instr) {
+		
+		std::vector<uint64_t> indices = instr->get_index();
+		for(std::vector<uint64_t>::iterator it = indices.begin(); it != indices.end(); ++it) {
+			//-- TODO Generate cache line requests
+			//std::shared_ptr<MCPUInstruction> mem_op = std::make_shared<MCPUInstruction>(*instr);
+			//
+			//mem_op->set_baseAddress(instr->get_baseAddress() + *it);
+			//mem_req_pipeline.push(mem_op);
+		}
+	}
+	
+	void MemoryCPUWrapper::memOp_orderedIndex(std::shared_ptr<MCPUInstruction> instr) {
+		memOp_nonUnit(instr);	// there is no difference
+	}
+	
+	void MemoryCPUWrapper::memOp_unorderedIndex(std::shared_ptr<MCPUInstruction> instr) {
+		memOp_nonUnit(instr);	// there is no difference
+	}
 
 
 	/////////////////////////////////////
 	//-- Command Execution
 	/////////////////////////////////////
-	void MemoryCPUWrapper::issueMCPU_() {
-            std::shared_ptr<MCPUSetVVL> mes = mcpu_req.front();
-	    mes->setVVL(mes->getAVL());
-	    std::cout << "MCPU: Returning VVL " << mes->getVVL() << " to core " << mes->getCoreId() << std::endl;
-	    mes->setServiced();
-	    out_port_noc_.send(std::make_shared<NoCMessage>(mes, NoCMessageType::MCPU_REQUEST, line_size_, mes->getMemoryCPU(), mes->getSourceTile()), 0);
+	/*void MemoryCPUWrapper::issueMCPU_() {
+		std::shared_ptr<MCPUSetVVL> mes = mcpu_req.front();
+		mes->setReturnedVecLen(mes->getRequestedVecLen());
+		std::cout << "MCPU: Returning VVL " << mes->getReturnedVecLen() << " to core " << mes->getCoreId() << std::endl;
+		mes->setServiced();
+		out_port_noc_.send(std::make_shared<NoCMessage>(mes, NoCMessageType::MCPU_REQUEST, line_size_, mes->getMemoryCPU(), mes->getSourceTile()), 0);
 
-            //-- Are there any messages left in the queue?
-            mcpu_req.pop_front();
-	    if(mcpu_req.size() > 0) {
-	        issue_mcpu_event_.schedule(1);
-	    }
-	}
-
+		//-- Are there any messages left in the queue?
+		mcpu_req.pop_front();
+		if(mcpu_req.size() > 0) {
+			issue_mcpu_event_.schedule(1);
+		}
+	}*/
 
 
 
@@ -100,9 +205,16 @@ namespace spike_model {
 	/////////////////////////////////////
 	void MemoryCPUWrapper::receiveMessage_mc_(const std::shared_ptr<CacheRequest> &mes)	{
 		count_requests_mc_++;
-		//std::cout << "Returning: " << mes << ", coreID: " << mes->getCoreId() << std::endl;
+		std::cout << "Returning: " << mes << ", coreID: " << mes->getCoreId() << std::endl;
 		mes->setServiced();
-		out_port_noc_.send(std::make_shared<NoCMessage>(mes, NoCMessageType::MEMORY_ACK, line_size_, mes->getMemoryController(), mes->getHomeTile()), 0);
+		
+		
+		//-- If the ID of the reply is < -, the cache line is for the MCPU and not for the VAS Tile
+		if(mes->getCoreId() != (uint16_t)-1) {
+			out_port_noc_.send(std::make_shared<NoCMessage>(mes, NoCMessageType::MEMORY_ACK, line_size_, mes->getMemoryController(), mes->getHomeTile()), 0);
+		} else {
+			std::cout << "For the MCPU" << std::endl;
+		}
 	}
 
 
--- SpikeModel/src/MemoryTile/MemoryCPUWrapper.hpp
+++ SpikeModel/src/MemoryTile/MemoryCPUWrapper.hpp
@@ -1,20 +1,22 @@
 #ifndef __MEMORY_CPU_WRAPPER_H__
 #define __MEMORY_CPU_WRAPPER_H__
 
-#include "sparta/ports/PortSet.hpp"
-#include "sparta/ports/SignalPort.hpp"
+#include <queue>
+
+//#include "sparta/ports/PortSet.hpp"
+//#include "sparta/ports/SignalPort.hpp"
 #include "sparta/ports/DataPort.hpp"
-#include "sparta/events/EventSet.hpp"
+//#include "sparta/events/EventSet.hpp"
 #include "sparta/events/UniqueEvent.hpp"
-#include "sparta/simulation/Unit.hpp"
+//#include "sparta/simulation/Unit.hpp"
 #include "sparta/simulation/ParameterSet.hpp"
 #include "sparta/simulation/TreeNode.hpp"
-#include "sparta/collection/Collectable.hpp"
-#include "sparta/events/StartupEvent.hpp"
-#include "sparta/resources/Pipeline.hpp"
-#include "sparta/resources/Buffer.hpp"
-#include "sparta/pairs/SpartaKeyPairs.hpp"
-#include "sparta/simulation/State.hpp"
+//#include "sparta/collection/Collectable.hpp"
+//#include "sparta/events/StartupEvent.hpp"
+//#include "sparta/resources/Pipeline.hpp"
+//#include "sparta/resources/Buffer.hpp"
+//#include "sparta/pairs/SpartaKeyPairs.hpp"
+//#include "sparta/simulation/State.hpp"
 #include "sparta/utils/SpartaSharedPointer.hpp"
 
 #include "LogCapable.hpp"
@@ -23,12 +25,14 @@
 #include "EventManager.hpp"
 #include "MCPUSetVVL.hpp"
 #include "MCPUInstruction.hpp"
+#include "CacheRequest.hpp"
 
 namespace spike_model {
 	class MemoryCPUWrapper : public sparta::Unit, public LogCapable, public spike_model::EventVisitor {
 		public:
 
 			using spike_model::EventVisitor::handle; //This prevents the compiler from warning on overloading
+
 
 			/*!
 			 * \class MemoryCPUWrapperParameterSet
@@ -38,7 +42,7 @@ namespace spike_model {
 				public:
 					//! Constructor for MemoryCPUParameterSet
 					MemoryCPUWrapperParameterSet(sparta::TreeNode* n):sparta::ParameterSet(n) { }
-                	PARAMETER(uint64_t, line_size, 128, "The cache line size")
+					PARAMETER(uint64_t, line_size, 128, "The cache line size")
 					PARAMETER(uint64_t, latency, 100, "The latency in the memory CPU wrapper")
 			};
 
@@ -68,21 +72,21 @@ namespace spike_model {
 
 		private:
 
-			uint64_t line_size_;
+			uint32_t line_size_;
+			uint32_t vvl_;
 			uint64_t latency_;
 
-
 			//-- commands coming from the tile
-			void issueMCPU_();
-			sparta::UniqueEvent<> issue_mcpu_event_ {
-				&unit_event_set_, "issue_mcpu_", CREATE_SPARTA_HANDLER(MemoryCPUWrapper, issueMCPU_)
-			};
-			std::list<std::shared_ptr<MCPUSetVVL>> mcpu_req;
+			//sparta::UniqueEvent<> issue_mcpu_event_ {
+			//	&unit_event_set_, "issue_mcpu_", CREATE_SPARTA_HANDLER(MemoryCPUWrapper, issueMCPU_)
+			//};
+			//std::list<std::shared_ptr<MCPUSetVVL>> mcpu_req;
+			//void issueMCPU_();
 
 
 			//-- message handling
-			bool idle_noc = true;
-			bool idle_mc  = true;
+			bool mcpu_incoming_idle;
+			
 			sparta::DataOutPort<std::shared_ptr<NoCMessage>> out_port_noc_ {
 				&unit_port_set_, "out_noc"
 			};
@@ -98,26 +102,50 @@ namespace spike_model {
 			sparta::DataInPort<std::shared_ptr<CacheRequest>> in_port_mc_ {
 				&unit_port_set_, "in_mc"
 			};
+			
+			
+			
+			
+			
+			sparta::UniqueEvent<sparta::SchedulingPhase::PostTick> controller_cycle_event_ {
+					&unit_event_set_, "controller_cycle_", CREATE_SPARTA_HANDLER(MemoryCPUWrapper, controllerCycle_)
+			};
+			
+			std::queue<std::shared_ptr<MCPUInstruction>> sched_incoming;
+			std::queue<std::shared_ptr<CacheRequest>> mem_req_pipeline;
+
 
 			std::shared_ptr<EventManager> request_manager_;
+			
+			
 			void receiveMessage_noc_(const std::shared_ptr<NoCMessage> &mes);
 			void receiveMessage_mc_(const std::shared_ptr<CacheRequest> &mes);
+			
+			/*!
+			 * \brief Handles an instruction forwarded to the MCPU
+			 * \param r The instruction to handle
+			 */
 			virtual void handle(std::shared_ptr<spike_model::CacheRequest> r) override;
 			virtual void handle(std::shared_ptr<spike_model::MCPUSetVVL> r) override;
-            
-            /*!
-             * \brief Handles an instruction forwarded to the MCPU
-             * \param r The instruction to handle
-             */
-            virtual void handle(std::shared_ptr<spike_model::MCPUInstruction> r) override;
+			virtual void handle(std::shared_ptr<spike_model::MCPUInstruction> r) override;
 
+
+			void controllerCycle_();
+			void schedule_incoming_mem_ops();
+			void schedule_outgoing_mem_ops();
+			void schedule_mem_ops_to_mc();
+			void memOp_unit(std::shared_ptr<MCPUInstruction> instr);
+			void memOp_nonUnit(std::shared_ptr<MCPUInstruction> instr);
+			void memOp_orderedIndex(std::shared_ptr<MCPUInstruction> instr);
+			void memOp_unorderedIndex(std::shared_ptr<MCPUInstruction> instr);
+			
 
 			//-- reporting and logging
 			sparta::Counter count_requests_noc_=sparta::Counter(getStatisticSet(), "requests_noc", "Number of requests from NoC", sparta::Counter::COUNT_NORMAL);
 			sparta::Counter count_load_=sparta::Counter(getStatisticSet(), "requests_noc_load", "Number of requests from NoC", sparta::Counter::COUNT_NORMAL);
 			sparta::Counter count_store_=sparta::Counter(getStatisticSet(), "requests_noc_store", "Number of requests from NoC", sparta::Counter::COUNT_NORMAL);
 			sparta::Counter count_requests_mc_=sparta::Counter(getStatisticSet(), "requests_mc", "Number of requests from MC", sparta::Counter::COUNT_NORMAL);
-
+			
 	};
 }
 #endif
