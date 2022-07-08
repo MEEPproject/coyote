4e8938f7099e4d927e5023fce886c0f984438005
Borja <borja.perez1@bsc.es>
2022-07-08 10:30:38 +0200

    Adds the axpy and matmul versions with compute-acces overlap and load reordering from Jens
    These features are enabled at compile time using defines

--- apps/axpy/axpy.c
+++ apps/axpy/axpy.c
@@ -1,5 +1,13 @@
 #include <assert.h>
 
+#define _MM_LOAD_f64        __builtin_epi_vload_1xf64
+#define _MM_MACC_f64        __builtin_epi_vfmacc_1xf64
+#define _MM_STORE_f64       __builtin_epi_vstore_1xf64
+#define FENCE()   asm volatile( "fence" : : );
+#define _MM_SET_f64         __builtin_epi_vfmv_v_f_1xf64
+
+#define OVERLAP_COMPUTE_AND_LOAD
+
 void axpy_intrinsics(int coreid, int ncores, int a, double *dx, double *dy, long n) {
   int i;
 
@@ -8,8 +16,147 @@ void axpy_intrinsics(int coreid, int ncores, int a, double *dx, double *dy, long
   int end = (coreid==ncores-1) ?  n : start+chunk;
 
   long gvl = __builtin_epi_vsetvl(chunk, __epi_e64, __epi_m1);
-  __epi_1xi64 v_a = __builtin_epi_vmv_v_x_1xi64(a, gvl);
+#ifdef OVERLAP_COMPUTE_AND_LOAD
+  int j, i0, i1, i2, i3, i4, i5, i6, i7;
+  __epi_1xf64 v_a = _MM_SET_f64(a, gvl);
+  j=start;
+  __epi_1xf64 vc0_dx;
+  __epi_1xf64 vc1_dy;
+  __epi_1xf64 vc2_dx;
+  __epi_1xf64 vc3_dy;
+  __epi_1xf64 vc4_dx;
+  __epi_1xf64 vc5_dy;
+  __epi_1xf64 vc6_dx;
+  __epi_1xf64 vc7_dy;
+  __epi_1xf64 vc8_dx;
+  __epi_1xf64 vc9_dy;
+  __epi_1xf64 vc10_dx;
+  __epi_1xf64 vc11_dy;
+  __epi_1xf64 vc12_dx;
+  __epi_1xf64 vc13_dy;
+  __epi_1xf64 vc14_dx;
+  __epi_1xf64 vc15_dy;
+  if (j + 8* gvl < end)
+   {
+    vc0_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc1_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc2_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc3_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc4_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc5_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc6_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc7_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc8_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc9_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc10_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc11_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc12_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc13_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+    vc14_dx = _MM_LOAD_f64(&dx[j], gvl);
+    vc15_dy = _MM_LOAD_f64(&dy[j], gvl);
+    j += gvl;
+   }
 
+    for (i = start; i < end;) 
+    {
+      if ((j + 8* gvl) < end)
+      {
+        // 8x copy into vector reg and start load before compute
+        __epi_1xf64 vb0_dx = vc0_dx;
+        __epi_1xf64 vb1_dy = vc1_dy;
+        __epi_1xf64 vc0_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc1_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb1_dy = _MM_MACC_f64(vb1_dy, v_a, vb0_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb1_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb2_dx = vc2_dx;
+        __epi_1xf64 vb3_dy = vc3_dy;
+        __epi_1xf64 vc2_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc3_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb3_dy = _MM_MACC_f64(vb3_dy, v_a, vb2_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb3_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb4_dx = vc4_dx;
+        __epi_1xf64 vb5_dy = vc5_dy;
+        __epi_1xf64 vc4_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc5_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb5_dy = _MM_MACC_f64(vb5_dy, v_a, vb4_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb5_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb6_dx = vc6_dx;
+        __epi_1xf64 vb7_dy = vc7_dy;
+        __epi_1xf64 vc6_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc7_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb7_dy = _MM_MACC_f64(vb7_dy, v_a, vb6_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb7_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb8_dx = vc8_dx;
+        __epi_1xf64 vb9_dy = vc9_dy;
+        __epi_1xf64 vc8_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc9_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb9_dy = _MM_MACC_f64(vb9_dy, v_a, vb8_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb9_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb10_dx = vc10_dx;
+        __epi_1xf64 vb11_dy = vc11_dy;
+        __epi_1xf64 vc10_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc11_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb11_dy = _MM_MACC_f64(vb11_dy, v_a, vb10_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb11_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb12_dx = vc12_dx;
+        __epi_1xf64 vb13_dy = vc13_dy;
+        __epi_1xf64 vc12_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc13_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb13_dy = _MM_MACC_f64(vb13_dy, v_a, vb12_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb13_dy, gvl);
+        i += gvl;
+
+        __epi_1xf64 vb14_dx = vc14_dx;
+        __epi_1xf64 vb15_dy = vc15_dy;
+        __epi_1xf64 vc14_dx = _MM_LOAD_f64(&dx[j], gvl);
+        __epi_1xf64 vc15_dy = _MM_LOAD_f64(&dy[j], gvl);
+        j += gvl;
+        vb15_dy = _MM_MACC_f64(vb15_dy, v_a, vb14_dx, gvl);
+        _MM_STORE_f64(&dy[i], vb15_dy, gvl);
+        i += gvl;
+
+      }
+      else
+      {
+        gvl = __builtin_epi_vsetvl(end - i, __epi_e64, __epi_m1);
+        __epi_1xf64 v_dx = _MM_LOAD_f64(&dx[i], gvl);
+        __epi_1xf64 v_dy = _MM_LOAD_f64(&dy[i], gvl);
+        __epi_1xf64 v_res = _MM_MACC_f64(v_dy, v_a, v_dx, gvl);
+        _MM_STORE_f64(&dy[i], v_res, gvl);
+
+        i += gvl;
+      }
+    }
+
+
+#else
+  __epi_1xi64 v_a = __builtin_epi_vmv_v_x_1xi64(a, gvl);
   for (i = start; i < end;) {
     gvl = __builtin_epi_vsetvl(end-i, __epi_e64, __epi_m1);
     __epi_1xf64 v_dx = __builtin_epi_vload_1xf64(&dx[i], gvl);
@@ -19,4 +166,5 @@ void axpy_intrinsics(int coreid, int ncores, int a, double *dx, double *dy, long
 
     i += gvl;
   }
+#endif
 }
--- apps/mt-matmul-vec/matmul.c
+++ apps/mt-matmul-vec/matmul.c
@@ -7,7 +7,8 @@
 #include "dataset.h"
 #include "util.h"
 
-//#define OVERLAP_COMPUTE_AND_LOAD
+#define OVERLAP_COMPUTE_AND_LOAD
+#define REORDER_FLDS
 
 #if 0
 #define FMA( vc, vb, sa, gvl ) do { \
@@ -41,7 +42,41 @@ static void matmul_builtins(int coreid, int ncores, int M, int K, int N, int bi,
 
 #ifdef OVERLAP_COMPUTE_AND_LOAD
         __epi_1xf64 vb1;
+        double fld0;
+        double fld1;
+        double fld2;
+        double fld3;
+        double fld4;
+        double fld5;
+        double fld6;
+        double fld7;
+        double fld8;
+        double fld9;
+        double fld10;
+        double fld11;
+        double fld12;
+        double fld13;
+        double fld14;
+        double fld15;
+        double fld16;
+        double fld17;
+        double fld18;
+        double fld19;
+        double fld20;
+        double fld21;
+        double fld22;
+        double fld23;
+        double fld24;
+        double fld25;
+        double fld26;
+        double fld27;
+        double fld28;
+        double fld29;
+        double fld30;
+        double fld31;
 #endif
+
+
         vc0  = __builtin_epi_vfmv_v_f_1xf64(0.0, gvl);
 
 #if 0
@@ -65,20 +100,108 @@ static void matmul_builtins(int coreid, int ncores, int M, int K, int N, int bi,
 #endif
 
 #ifdef OVERLAP_COMPUTE_AND_LOAD
+        // Load for kk=0
         vb1 = __builtin_epi_vload_1xf64(&b[0][jj], gvl);
+        fld16=a[ii][0];
+        fld17=a[ii+1][0];
+        fld18=a[ii+2][0];
+        fld19=a[ii+3][0];
+        fld20=a[ii+4][0];
+        fld21=a[ii+5][0];
+        fld22=a[ii+6][0];
+        fld23=a[ii+7][0];
+        fld24=a[ii+8][0];
+        fld25=a[ii+9][0];
+        fld26=a[ii+10][0];
+        fld27=a[ii+11][0];
+        fld28=a[ii+12][0];
+        fld29=a[ii+13][0];
+        fld30=a[ii+14][0];
+        fld31=a[ii+15][0];
 #endif
+
 
         for (int kk = 0; kk < K; kk += bk) {
 #ifdef OVERLAP_COMPUTE_AND_LOAD
+         if (kk + bk < K) 
+         {
            vb0=vb1;
-           if (kk + 1 < K) 
-           {
-              vb1 = __builtin_epi_vload_1xf64(&b[kk+1][jj], gvl);
-           }
+           vb1 = __builtin_epi_vload_1xf64(&b[kk+bk][jj], gvl);
+           fld0=fld16;
+           fld16=a[ii][kk+bk];
+           FMA( vc0,  vb0,  fld0, gvl ); //vc0 calculates the first  value of row0
+           fld1=fld17;
+           fld17=a[ii+1][kk+bk];
+           FMA( vc1,  vb0,  fld1, gvl ); //vc1 calculates the first  value of row1
+           fld2=fld18;
+           fld18=a[ii+2][kk+bk];
+           FMA( vc2,  vb0,  fld2, gvl );
+           fld3=fld19;
+           fld19=a[ii+2][kk+bk];
+           FMA( vc3,  vb0,  fld3, gvl );
+           fld4=fld20;
+           fld20=a[ii+2][kk+bk];
+           FMA( vc4,  vb0,  fld4, gvl );
+           fld5=fld21;
+           fld21=a[ii+2][kk+bk];
+           FMA( vc5,  vb0,  fld5, gvl );
+           fld6=fld22;
+           fld22=a[ii+2][kk+bk];
+           FMA( vc6,  vb0,  fld6, gvl );
+           fld7=fld23;
+           fld23=a[ii+2][kk+bk];
+           FMA( vc7,  vb0,  fld7, gvl );
+           fld8=fld24;
+           fld24=a[ii+2][kk+bk];
+           FMA( vc8,  vb0,  fld8, gvl );
+           fld9=fld25;
+           fld25=a[ii+2][kk+bk];
+           FMA( vc9,  vb0,  fld9, gvl );
+           fld10=fld26;
+           fld26=a[ii+2][kk+bk];
+           FMA( vc10, vb0,  fld10, gvl );
+           fld11=fld27;
+           fld27=a[ii+2][kk+bk];
+           FMA( vc11, vb0,  fld11, gvl );
+           fld12=fld28;
+           fld28=a[ii+2][kk+bk];
+           FMA( vc12, vb0,  fld12, gvl );
+           fld13=fld29;
+           fld29=a[ii+2][kk+bk];
+           FMA( vc13, vb0,  fld13, gvl );
+           fld14=fld30;
+           fld30=a[ii+2][kk+bk];
+           FMA( vc14, vb0,  fld14, gvl );
+           fld15=fld31;
+           fld31=a[ii+2][kk+bk];
+           FMA( vc15, vb0,  fld15, gvl );
+          }
+         else
+         { // Last elements
+           FMA( vc0,  vb1,  fld16, gvl ); //vc0 calculates the first  value of row0
+           FMA( vc1,  vb1,  fld17, gvl ); //vc1 calculates the first  value of row1
+           FMA( vc2,  vb1,  fld18, gvl );   
+           FMA( vc3,  vb1,  fld19, gvl );    
+           FMA( vc4,  vb1,  fld20, gvl );   
+           FMA( vc5,  vb1,  fld21, gvl );   
+           FMA( vc6,  vb1,  fld22, gvl );         
+           FMA( vc7,  vb1,  fld23, gvl );      
+           FMA( vc8,  vb1,  fld24, gvl );           
+           FMA( vc9,  vb1,  fld25, gvl );     
+           FMA( vc10, vb1,  fld26, gvl );         
+           FMA( vc11, vb1,  fld27, gvl );         
+           FMA( vc12, vb1,  fld28, gvl );        
+           FMA( vc13, vb1,  fld29, gvl );        
+           FMA( vc14, vb1,  fld30, gvl );         
+           FMA( vc15, vb1,  fld31, gvl );
+
+         }
+
 #else
+
            vb0 = __builtin_epi_vload_1xf64(&b[kk][jj], gvl);
-#endif
-	   {
+	   
+
            FMA( vc0,  vb0,   a[ii][kk], gvl ); //vc0 calculates the first  value of row0
            FMA( vc1,  vb0,  a[ii+1][kk], gvl ); //vc1 calculates the first  value of row1
            FMA( vc2,  vb0,  a[ii+2][kk], gvl );
@@ -95,7 +218,8 @@ static void matmul_builtins(int coreid, int ncores, int M, int K, int N, int bi,
            FMA( vc13, vb0, a[ii+13][kk], gvl );
            FMA( vc14, vb0, a[ii+14][kk], gvl );
            FMA( vc15, vb0, a[ii+15][kk], gvl );
-	   }
+#endif
+	   
         }
         __builtin_epi_vstore_1xf64(&c[ii][jj], vc0, gvl);
         __builtin_epi_vstore_1xf64(&c[ii+1][jj], vc1, gvl);
